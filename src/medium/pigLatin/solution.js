48e4d776f9cda6d97669efbee159496c60dc8139
Joel Watkins <eck087@yahoo.co.uk>
2022-09-16 17:03:26 +0100

    pig latin pseudocode update

--- src/medium/pigLatin/solution.js
+++ src/medium/pigLatin/solution.js
@@ -2,7 +2,12 @@
 //   convert string to an array - split method
 //   define an empty array
 
-//   for loop- through the array. In each iteration, we push a slice 
+//   for loop- through the array
+//     in each iteration, we push to the empty array - first the rest of the word, then the first letter, then the 'ay' string
+
+//   We join the array back to a string
+
+//   return the first letter capitalized followed by the remainder
 
 function pigLatinConverter(string) {
   const arr = string.toLowerCase().split(' ')
